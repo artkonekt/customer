057f214a333780784d902378556f22be619489c5
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2022-03-10 22:11:21 +0100

    Added Enum v4 support, dropped PHP 7.3 & 7.4, Laravel 6 & 7
    - Removed travis
    - Added github actions

--- Changelog.md
+++ Changelog.md
@@ -1,6 +1,15 @@
 # Changelog
 ### Konekt Customer Module
 
+## Unreleased
+##### 2022-03-XX
+
+- Added Enum v4 support
+- Dropped PHP 7.3 & 7.4 support
+- Dropped Laravel 6 & 7 support
+- Changed minimum Laravel version is 8.22.1, to enforce the [CVE-2021-21263](https://blog.laravel.com/security-laravel-62011-7302-8221-released) security patch
+- Changed CI from Travis to Github Actions
+
 ## 2.1.0
 ##### 2020-12-07
 
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -58,11 +58,12 @@ abstract class TestCase extends Orchestra
         $app['config']->set('database.default', $engine);
         $app['config']->set('database.connections.' . $engine, [
             'driver'   => $engine,
-            'database' => 'sqlite' == $engine ? ':memory:' : 'customer_test',
+            'database' => 'sqlite' == $engine ? ':memory:' : 'address_test',
             'prefix'   => '',
-            'host'     => '127.0.0.1',
+            'host'     => env('TEST_DB_HOST', '127.0.0.1'),
+            'port'     => env('TEST_DB_PORT'),
             'username' => env('TEST_DB_USERNAME', 'pgsql' === $engine ? 'postgres' : 'root'),
-            'password' => env('TEST_DB_PASSWORD', ''),
+            'password' => env('TEST_DB_PASSWORD', '')
         ]);
 
         if ('pgsql' === $engine) {
